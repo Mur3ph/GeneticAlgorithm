ba9ce7615b6c8925ff35dcd0d4a39c2fe98289e7
Mur3ph <paulmurphymainstreet@gmail.com>
2015-05-09 19:52:22 +0100

    commit

---
+++ src/ie/murph/java/data/IntegerData.java
@@ -0,0 +1,5 @@
+package ie.murph.java.data;
+
+public class IntegerData {
+
+}
--- src/ie/murph/java/interfaces/MapValueComparator.java
+++ src/ie/murph/java/interfaces/MapValueComparator.java
@@ -5,15 +5,16 @@ import java.util.Map;
 
 public class MapValueComparator implements Comparator<String> 
 {
-	Map<String, Integer> m_baseLocaleMap;
-	public MapValueComparator(Map<String, Integer> baseMapPassedIn)
+	private Map<String, Integer> m_baseLocaleMap;
+	public MapValueComparator(Map<String, Integer> baseMapDataPassedIn)
 	{
-		m_baseLocaleMap = baseMapPassedIn;
+		m_baseLocaleMap = baseMapDataPassedIn;
 	}
 	
 	@Override
 	public int compare(String comparableData1, String comparableData2) 
 	{
+		//Arranging the data in order from the Map value instead of the Maps key as is by default
 		if(m_baseLocaleMap.get(comparableData1) >= m_baseLocaleMap.get(comparableData2))
 		{
 			return -1;
