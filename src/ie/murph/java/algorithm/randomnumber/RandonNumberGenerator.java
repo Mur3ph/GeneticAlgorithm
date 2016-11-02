060f69cffbbeb93aab1af7e148cda2b266709c89
Mur3ph <paulmurphymainstreet@gmail.com>
2016-11-02 12:20:44 +0000

    restructured aa package to seperate code logic

--- src/ie/murph/java/algorithm/randomnumber/RandonNumberGenerator.java
+++ src/ie/murph/java/algorithm/randomnumber/RandonNumberGenerator.java
@@ -1,4 +1,4 @@
-package ie.murph.java.algorithm;
+package ie.murph.java.algorithm.randomnumber;
 
 import java.security.SecureRandom;
 import java.util.Random;
@@ -6,24 +6,24 @@ import java.util.Random;
 public class RandonNumberGenerator 
 {
 	private static final Random RANDOM_NUMBER_GENERATOR = new SecureRandom();
-	private Integer randomNumber;
-	private Integer[] randomNumbers;
+	private int randomNumber;
+	private int[] randomNumbers;
 	
 	public RandonNumberGenerator()
 	{
 		
 	}
 	
-	public void generateARandomNumber(int maxRangeOfRandomNumber) 
+	public void generateARandomWholeNumber(int maxRangeOfRandomNumber) 
     {
 		final int minRangeOfRandomNumber = 1;
 		final int one = 1;
 		randomNumber = RANDOM_NUMBER_GENERATOR.nextInt(maxRangeOfRandomNumber - minRangeOfRandomNumber + one) + minRangeOfRandomNumber;
 	}
 	
-	public void populateWithRandomNumbers(int lenghtOfArray)
+	public void populateWithRandomWholeNumbers(int lenghtOfArray)
 	{
-		randomNumbers = new Integer[lenghtOfArray];
+		randomNumbers = new int[lenghtOfArray];
 		for(int atPositionX = 0; atPositionX < lenghtOfArray; atPositionX++)
 		{
 			randomNumbers[atPositionX] = randomNumber;
@@ -31,11 +31,14 @@ public class RandonNumberGenerator
 	}
 		
 	// Method to get random integers for the fitness
-	public Integer[] getRandomNumers()
+	public int[] getRandomWholeNumbers()
 	{
 		return randomNumbers;
 	}
 	
-	
+	public double getRandomDecimalNumberBetweenZeroAndOne() 
+    {
+		return RANDOM_NUMBER_GENERATOR.nextDouble();
+	}
 	
 }
