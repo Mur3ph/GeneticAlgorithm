581ec86cb305a063ccdd6a5c1a993896a3adae05
Mur3ph <paulmurphymainstreet@gmail.com>
2016-12-28 14:25:54 +0000

    Renaming variables and methds

--- src/ie/murph/java/util/BinaryUtil.java
+++ src/ie/murph/java/util/BinaryUtil.java
@@ -4,32 +4,36 @@ import ie.murph.java.algorithm.math.CumulativeFrequency;
 
 public class BinaryUtil 
 {
+//	static int bit128 = 128; 	// for 8 bit we use 128      (i.e. 7 bit = 128)
+	static int bit32 = 32; 		// for 6 bit we use 32 etc.  (i.e. 5 bit = 32)
+//	static int bit16 = 16; 		// for 5 bit we use 32 etc.  (i.e. 4 bit = 32)
+	
 	static char negativeBinaryBit = '0';
 	static char positiveBinaryBit = '1';
 	
 	// Method to find the (6 bit) binary equivalent of an integer - or change the length of loops to whatever size bit you need
-	public static String convertIntegerToBinaryString(int numValue, int lengthOfBinaryString) 
+	public static String convertIntegerToBinaryString(int integerValue, int lengthOfBinaryString) 
 	{
 		String binary = "";
 		for(int x = 0; x < lengthOfBinaryString; x++)
 		{
 			//If the value has a remainder use the '1' binary bit
-			if(hasRemainder(numValue))
+			if(hasRemainder(integerValue))
 			{
 				binary = appendPositiveBinaryBit(binary);
 			}
-			if(hasNoRemainder(numValue))
+			if(hasNoRemainder(integerValue))
 			{
 				binary = appendNegativeBinaryBit(binary);
 			}
-			numValue = half(numValue);
+			integerValue = half(integerValue);
 		}
 		return binary;
 	}
 	
-	private static boolean hasRemainder(int numberValue) 
+	private static boolean hasRemainder(int integerValue) 
 	{
-		return numberValue % 2 == 1;
+		return integerValue % 2 == 1;
 	}
 
 	private static String appendPositiveBinaryBit(String binary)
@@ -37,9 +41,9 @@ public class BinaryUtil
 		return "1" + binary;
 	}
 	
-	private static boolean hasNoRemainder(int numberValue) 
+	private static boolean hasNoRemainder(int integerValue) 
 	{
-		return numberValue % 2 == 0;
+		return integerValue % 2 == 0;
 	}
 	
 	private static String appendNegativeBinaryBit(String binary)
@@ -47,31 +51,28 @@ public class BinaryUtil
 		return "0" + binary;
 	}
 	
-	private static int half(int value) 
+	private static int half(int integerValue) 
 	{
-		return value/2;
+		return integerValue/2;
 	}
 	
 	//Converting my binary bits back to Integer
 	public static int convertBinaryToInteger(String binary)
 	{
-//		int x = 128; // for 8 bit we use 128      (i.e. 7 bit = 128)
-		int bit32 = 32; // for 6 bit we use 32 etc.   (i.e. 5 bit = 32)
-//		int a = 16; // for 5 bit we use 32 etc.   (i.e. 4 bit = 32)
-		int result = 0;
+		int integer = 0;
 		for(int atPositionX = 0; atPositionX < 6; atPositionX++)
 		{
 			char character = binary.charAt(atPositionX);
 			// Single quotations needed when using 'char'
-			if(character == '1')
+			if(character == positiveBinaryBit)
 			{
 				//Only if the binary bit is a one do we calculate sum below
-				result = result + (bit32*1);
+				integer = integer + (bit32*1);
 			}
 			//We are working from the left of the binary string to the right, so we start at 32, then half for 16, next 8, and so on
 			bit32=bit32/2;
 		}
-		return result;
+		return integer;
 	}
 	
 	// Altering one of the bits in the binary string using the StringBuilder Object
@@ -97,7 +98,7 @@ public class BinaryUtil
 		//If it is a zero binary bit change to a one and so on..
 		for(int atPositionX = 0; atPositionX < bothOffspringBinaryBuilderStringsToBeAlteredArray.length; atPositionX++)
 		{
-			if(bothbinaryBitsOfOffspringStringToBeAltered[atPositionX] == '0')
+			if(bothbinaryBitsOfOffspringStringToBeAltered[atPositionX] == negativeBinaryBit)
 			{
 				bothOffspringBinaryBuilderStringsToBeAlteredArray[atPositionX].setCharAt(bothrandomNumbersOfPositionsOfBinaryBitToBeAltered[atPositionX], positiveBinaryBit);
 				strBuilderArrayWithBothAlteredBinaryCodes[atPositionX] = bothOffspringBinaryBuilderStringsToBeAlteredArray[atPositionX];
