00bfa7e3fdae40e5dea897f59fa75777a4a8b6b4
Mur3ph <paulmurphymainstreet@gmail.com>
2017-04-19 11:14:59 +0100

    Logger 4 Java log4j

--- src/ie/murph/java/main/logging/MyLogger.java
+++ src/ie/murph/java/main/logging/MyLogger.java
@@ -5,4 +5,27 @@ import org.apache.log4j.Logger;
 public class MyLogger
 {
     private final static Logger logger = Logger.getLogger(MyLogger.class);
+    
+    public static void main(String[] args) {
+
+	MyLogger obj = new MyLogger();
+	obj.runMe("pauly");
+
+    }
+
+    private void runMe(String parameter)
+    {
+	if(logger.isDebugEnabled()){
+		logger.debug("This is debug : " + parameter);
+	}
+
+	if(logger.isInfoEnabled()){
+		logger.info("This is info : " + parameter);
+	}
+
+	logger.warn("This is warn : " + parameter);
+	logger.error("This is error : " + parameter);
+	logger.fatal("This is fatal : " + parameter);
+
+    }
 }
