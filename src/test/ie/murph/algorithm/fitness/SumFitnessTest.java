9e71b3e27afe4485a299941c04dc55b9801bab5f
Mur3ph <paulmurphymainstreet@gmail.com>
2018-07-19 11:41:13 +0100

    just formating

---
+++ src/test/ie/murph/algorithm/fitness/SumFitnessTest.java
@@ -0,0 +1,22 @@
+package test.ie.murph.algorithm.fitness;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SumFitnessTest
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
