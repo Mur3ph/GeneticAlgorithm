62e2099a78da2d40d4544222a9f9fca0b46ead84
Mur3ph <paulmurphymainstreet@gmail.com>
2018-07-19 11:41:54 +0100

    just formating again

--- src/main/ie/murph/algorithm/fitness/SumFitness.java
+++ src/main/ie/murph/algorithm/fitness/SumFitness.java
@@ -7,51 +7,51 @@ import org.apache.log4j.Logger;
 
 public class SumFitness
 {
-    private static final Logger LOGGER = Logger.getLogger(SumFitness.class);
-    private List<Integer> fitnessValuesFromOrderedTreemap;
-    private OrganizedFitness organizedFitness;
-    private double totalSumOfFitness = 0;
+	private static final Logger LOGGER = Logger.getLogger(SumFitness.class);
+	private List<Integer> fitnessValuesFromOrderedTreemap;
+	private OrganizedFitness organizedFitness;
+	private double totalSumOfFitness = 0;
 
-    public SumFitness(OrganizedFitness organizedFitness)
-    {
-	this.organizedFitness = organizedFitness;
-    }
-
-    public void populateListWithFitnessValues()
-    {
-	LOGGER.debug("++populateListWithFitnessValues()\n");
-	this.fitnessValuesFromOrderedTreemap = new ArrayList<Integer>(this.organizedFitness.getOrderedFitnessValues());
-	LOGGER.debug("--populateListWithFitnessValues()\n");
-    }
-
-    public List<Integer> getFitnessValuesList()
-    {
-	LOGGER.debug("++getFitnessValuesList()\n");
-	return this.fitnessValuesFromOrderedTreemap;
-    }
-
-    public void calculatingTotalSumOfFitness()
-    {
-	LOGGER.debug("++calculatingTotalSumOfFitness()\n");
-	double totalSumOfFitness = 0;
-	for (int nextFitness = 0; nextFitness < this.organizedFitness.getSizeOfMap(); nextFitness++)
+	public SumFitness(OrganizedFitness organizedFitness)
 	{
-	    totalSumOfFitness += this.fitnessValuesFromOrderedTreemap.get(nextFitness);
+		this.organizedFitness = organizedFitness;
 	}
-	setTotalSumOfFitness(totalSumOfFitness);
-	LOGGER.debug("--calculatingTotalSumOfFitness()\n");
-    }
 
-    private void setTotalSumOfFitness(double totalSumOfFitness)
-    {
-	LOGGER.debug("++setTotalSumOfFitness()\n");
-	this.totalSumOfFitness = totalSumOfFitness;
-	LOGGER.debug("--setTotalSumOfFitness()\n");
-    }
+	public void populateListWithFitnessValues()
+	{
+		LOGGER.debug("++populateListWithFitnessValues()\n");
+		this.fitnessValuesFromOrderedTreemap = new ArrayList<Integer>(this.organizedFitness.getOrderedFitnessValues());
+		LOGGER.debug("--populateListWithFitnessValues()\n");
+	}
 
-    public double getTotalSumOfFitness()
-    {
-	LOGGER.debug("++getTotalSumOfFitness()\n");
-	return this.totalSumOfFitness;
-    }
+	public List<Integer> getFitnessValuesList()
+	{
+		LOGGER.debug("++getFitnessValuesList()\n");
+		return this.fitnessValuesFromOrderedTreemap;
+	}
+
+	public void calculatingTotalSumOfFitness()
+	{
+		LOGGER.debug("++calculatingTotalSumOfFitness()\n");
+		double totalSumOfFitness = 0;
+		for (int nextFitness = 0; nextFitness < this.organizedFitness.getSizeOfMap(); nextFitness++)
+		{
+			totalSumOfFitness += this.fitnessValuesFromOrderedTreemap.get(nextFitness);
+		}
+		setTotalSumOfFitness(totalSumOfFitness);
+		LOGGER.debug("--calculatingTotalSumOfFitness()\n");
+	}
+
+	private void setTotalSumOfFitness(double totalSumOfFitness)
+	{
+		LOGGER.debug("++setTotalSumOfFitness()\n");
+		this.totalSumOfFitness = totalSumOfFitness;
+		LOGGER.debug("--setTotalSumOfFitness()\n");
+	}
+
+	public double getTotalSumOfFitness()
+	{
+		LOGGER.debug("++getTotalSumOfFitness()\n");
+		return this.totalSumOfFitness;
+	}
 }
